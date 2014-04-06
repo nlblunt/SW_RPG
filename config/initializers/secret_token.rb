953e4772cf403a705cd6fb6dbe208419d65ca59a
nlblunt <nicolas.blunt@gmail.com>
2014-04-06 09:54:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SWRpg::Application.config.secret_key_base = 'dc2381a79f725e84798974aabe8b37d9b0586be43f6b1c45899c2245539376a2fb8b54df7710d9106a4cbb8e7c5e35d856f8d94c61eebc102592024505bbc513'
