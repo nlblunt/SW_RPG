5c608eb344d4f8aba5c4205e075eff7cf9f9e416
Nicolas <nicolas.blunt@gmail.com>
2016-02-13 20:15:45 -0800

    Fix RSPEC for GM_Controller

--- app/controllers/gm_controller.rb
+++ app/controllers/gm_controller.rb
@@ -36,13 +36,13 @@ class GmController < ApplicationController
   
   def pc_modify_strain
     #Modify PC strain.  Can be + or -
-    pc = Pc.find_by_id(params[:id])
+    @pc = Pc.find_by_id(params[:id])
 
-    pc.modify_strain(params[:amount])
+    @pc.modify_strain(params[:amount])
     
     t = Time.now
     time = t.strftime("%I:%M:%S")
-    render status: :ok, json: {msg: pc.name + ": strain modified " + params[:amount].to_s, strain: pc.strain_current, time: time}
+    render status: :ok, json: {msg: @pc.name + ": strain modified " + params[:amount].to_s, strain: @pc.strain_current, time: time}
   end
   
   def pc_modify_wounds
